c7721b99c0a3c5ea38cd82cdb586af5fa70357fe
Aditya Narayanan <adityanarayanan.narayanan@gmail.com>
2019-04-17 14:50:39 -0700

    Added SFM

--- src/sfm.java
+++ src/sfm.java
@@ -1,7 +1,29 @@
 public class sfm {
+    private final int areaCode;
+    private final int phoneNum;
+
+    private sfm(int areaCode, int phoneNum){
+        this.areaCode = areaCode;
+        this.phoneNum = phoneNum;
+
+    }
 
     public static void main(String[] args){
-        System.out.println("Hellno");
-        //Proper Oh OkOK
+
+        new Main().main();
+
+        System.out.println("KLOL");
+    }
+
+    public static sfm of(int areaCode, int phoneNum) {
+        return new sfm(areaCode, phoneNum);
+    }
+}
+class Main{
+
+    public static void main(){
+        sfm test = sfm.of(12,3);
+        System.out.println(test);
+
     }
 }
