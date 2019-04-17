d96390d670688acd0fc37d55f9eb0a80f0232518
Aditya Narayanan <adityanarayanan.narayanan@gmail.com>
2019-04-17 16:41:57 -0700

    Added Formattable,Comparable Methods

--- src/sfm.java
+++ src/sfm.java
@@ -1,6 +1,15 @@
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.ComparisonChain;
+
+import java.util.Comparator;
+import java.util.Formattable;
+import java.util.Formatter;
+
 import static com.google.common.base.Preconditions.checkArgument;
 
-public class sfm {
+public class sfm implements Formattable,Comparable {
+
+
     private final int areaCode;
     private final int phoneNum;
 
@@ -13,8 +22,6 @@ public class sfm {
     public static void main(String[] args){
 
         new Main().main();
-
-        System.out.println("KLOL");
     }
 
     public static sfm of(int areaCode, int phoneNum) {
@@ -34,15 +41,52 @@ public class sfm {
         }
         return false;
     }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("areaCode",areaCode)
+                .add("phone number",phoneNum)
+                .toString();
+    }
+
+    @Override
+    public void formatTo(Formatter formatter, int flags, int width, int precision) {
+        formatter.format("%d-%d",areaCode,phoneNum);
+
+    }
+
+    @Override
+    public int compareTo(Object o) {
+
+            // Java API Implementation
+            /*
+            return Comparator.comparingInt((sfm a) -> a.areaCode)
+                    .thenComparingInt(a -> a.phoneNum)
+                    .compare(this,(sfm) o);
+            */
+            // GUAVA IMPLEMENTATION
+
+            return ComparisonChain.start()
+                    .compare(this.areaCode,((sfm) o).areaCode)
+                    .compare(this.phoneNum,((sfm) o).phoneNum)
+                    .result();
+
+
+
+
+    }
 }
 class Main{
 
     public static void main(){
-        sfm test = sfm.of(120000,3000);
+        sfm test = sfm.of(120000,30000);
         sfm eq = sfm.of(120000,3000);
         System.out.println(test.equals(eq));
 
-        System.out.println(test);
-        System.out.println();
+        System.out.println(test.toString());  // toString Implementation
+        System.out.println(String.format("%s",test));// Formattable Implementation
+
+        System.out.println(test.compareTo(eq)); // CompareTo Implementation
     }
 }
