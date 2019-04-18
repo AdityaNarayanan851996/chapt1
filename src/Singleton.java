d0109c969311921f1aed750292deb97624ec5e2a
Aditya <adityanarayanan.narayanan@gmail.com>
2019-04-17 22:50:03 -0700

    Added Singleton

---
+++ src/Singleton.java
@@ -0,0 +1,10 @@
+import java.io.Serializable;
+/*
+public class Singleton implements Serializable {
+    private static final Singleton INSTANCE = new Singleton();
+    Singleton(){}
+}
+*/
+public enum Singleton{
+    INSTANCE;
+}
